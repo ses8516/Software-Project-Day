c45bd03c471dabaa7d547157f4a0778546d8aab9
Lettuceman44 <sdneamtu@gmail.com>
2013-10-21 13:37:04 -0400

    Added methods in Manager.java for the various meetings and breaks(wait times for each meeting or break)

--- Software_project-Day/src/Manager.java
+++ Software_project-Day/src/Manager.java
@@ -1,4 +1,61 @@
+import java.util.Random;
 
-public class Manager {
+
+public class Manager extends Thread {
+	
+	private Thread manager;
+	public Manager(Thread manager){
+		this.manager = manager;
+	}
+	
+	/**
+	 * Manager has a standup meeting for 15 minutes
+	 */
+	public void standupMeeting(){
+		System.out.println("Manager is having a standup meeting.");
+		try {
+			this.wait(150);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * The manager takes a 1 hour our lunch break 
+	 */
+	public void lunchBreak(){
+		System.out.println("Manager is on lunch break");
+		try {
+			this.wait(600);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * The manager holds a 1 hour meeting
+	 */
+	public void executiveMeeting(){
+		System.out.println("Manager is in an executive meeting");
+		try {
+			this.wait(600);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * The manager holds a project status meeting for 15 minutes
+	 */
+	public void statusMeeting(){
+		System.out.println("Manager is conducting a Project Status meeting");
+		try {
+			this.wait(150);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 }
