c536fae5c1f63fa104732d676fd004df6cb732d3
Nevets44 <montiacon@gmail.com>
2013-10-23 15:58:53 -0400

    Update Clock.java

--- Software_project-Day/src/Clock.java
+++ Software_project-Day/src/Clock.java
@@ -1,28 +1,28 @@
 public class Clock extends Thread{
 		
-		volatile int time = 0;
+	volatile int time = 0;
 		
-		public Clock(){}
+	public Clock(){}
 		
-		public String getTime(){
-		  // Dividing by 600 gets the hour and the +8 adjusts it correctly
-		  // e.g 1 hr from the start of the day would be 9.
-			String hour = "" + ( (time/600) + 8 );
-			String minutes = "" + (time%600);
+	public String getTime(){
+		// Dividing by 600 gets the hour and the +8 adjusts it correctly
+		// e.g 1 hr from the start of the day would be 9.
+		String hour = "" + ( (time/600) + 8 );
+		String minutes = "" + (time%600);
 			
-			return hour + ":" + minutes;
-		}
+		return hour + ":" + minutes;
+	}
 		
-		public void run(){
-			for( int hr = 1; hr <= 9; hr++){
-				for( int min = 0; min < 60; min++){
+	public void run(){
+		for( int hr = 1; hr <= 9; hr++){
+			for( int min = 0; min < 60; min++){
+				try{
+					time += min;
 					//send out this hr and min to all interested parties.
-					try{
-					  time += min;
-					} catch (InterruptedException e) {
-			      e.printStackTrace();
-		      }
-		    }
+				} catch (InterruptedException e) {
+			      		e.printStackTrace();
+		      		}
 			}
 		}
 	}
+}
