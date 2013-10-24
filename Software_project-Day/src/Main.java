a87beaa97c3fe5789e463aa03f1cd3fcaf0dbc8f
Stefano <ses8516@rit.edu>
2013-10-24 01:47:21 -0400

    regular employees are now passed a team lead

--- Software_project-Day/src/Main.java
+++ Software_project-Day/src/Main.java
@@ -5,17 +5,17 @@ public class Main {
 	
 	public static void main(String[] args){
 		Clock clock = new Clock();
-		
 		Manager manager = new Manager(clock, 4);
-		
-		
 		ArrayList<Employee> employees = new ArrayList<Employee>(12);
-		// Create Teams
+		
+		// Create 3 Teams
 		for(int i = 1; i < 4; i++){
-			for(int j = 1; j < 5; j++){
+			Employee teamLead = new Employee(i,1,manager,clock);
+			employees.add(teamLead);
+			for(int j = 1; j < 4; j++){
 				// i = team number
 				// j = employee number
-				employees.add(new Employee(i,j,clock));
+				employees.add(new Employee(i,j,teamLead,manager,clock));
 				
 //				System.out.println(i+""+j);
 			}
@@ -23,9 +23,9 @@ public class Main {
 		
 		clock.start();
 		manager.start();
-//		for(Employee e : employees){
-//			e.start();
-//		}
+		for(Employee e : employees){
+			e.start();
+		}
 		
 	}
 }
